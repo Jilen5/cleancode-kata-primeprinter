27140200d0deb6c2b33d4bce6d85d67df0a1060a
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2024-12-13 11:00:40 +0100

    Amélioration exo1

---
+++ start/cleancode-kata-primeprinter/src/main/java/Exercise1.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+public class Exercise1 {
+
+    private final LocalDate expirationDate;
+    private final Boolean approvedForConsumption;
+    private final Integer inspectorId;
+
+    public Exercise1(LocalDate expirationDate, Boolean approvedForConsumption, Integer inspectorId) {
+        this.expirationDate = expirationDate;
+        this.approvedForConsumption = approvedForConsumption;
+        this.inspectorId = inspectorId;
+    }
+
+    public boolean isEdible()
+    {
+        if (this.expirationDate.isAfter(LocalDate.now()) &&
+                this.approvedForConsumption &&
+                this.inspectorId != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
